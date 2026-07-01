d9ecf60aa3c3c3f18837ed36c9bc2562a9c638a5
itsdapi <altgr3@foxmail.com>
2026-07-01 18:41:24 +0800

    fix(openbao): expose raft cluster listener on 0.0.0.0:8201
    OpenBao was advertising cluster_addr :8201 but only bound raft to
    127.0.0.1:8301, so followers could not complete join or unseal.

--- docker/openbao/openbao-entrypoint.sh
+++ docker/openbao/openbao-entrypoint.sh
@@ -7,22 +7,30 @@ listener "tcp" {
   tls_disable = true
 }
 
+listener "tcp" {
+  address     = "0.0.0.0:${CLUSTER_PORT}"
+  tls_disable = true
+}
+
 storage "raft" {
   path    = "/openbao/data"
   node_id = "${NODE_ID}"
 
   retry_join {
-    leader_api_addr = "https://${PEER1_HOSTNAME}:${OPENBAO_PORT}"
+    leader_api_addr     = "https://${PEER1_HOSTNAME}:${OPENBAO_PORT}"
+    leader_tls_servername = "${PEER1_HOSTNAME}"
   }
   retry_join {
-    leader_api_addr = "https://${PEER2_HOSTNAME}:${OPENBAO_PORT}"
+    leader_api_addr     = "https://${PEER2_HOSTNAME}:${OPENBAO_PORT}"
+    leader_tls_servername = "${PEER2_HOSTNAME}"
   }
   retry_join {
-    leader_api_addr = "https://${PEER3_HOSTNAME}:${OPENBAO_PORT}"
+    leader_api_addr     = "https://${PEER3_HOSTNAME}:${OPENBAO_PORT}"
+    leader_tls_servername = "${PEER3_HOSTNAME}"
   }
 }
 
-cluster_addr = "https://${NODE_HOSTNAME}:${CLUSTER_PORT}"
+cluster_addr = "http://${NODE_HOSTNAME}:${CLUSTER_PORT}"
 api_addr     = "https://${NODE_HOSTNAME}:${OPENBAO_PORT}"
 
 ui = true
